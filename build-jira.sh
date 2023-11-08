9b56351d1ed859c4c8fa55632e45a3a3f530ca93
Nobi <nobi@nobidev.com>
2023-11-08 13:46:55 +0700

    fixup! WIP

--- build-jira.sh
+++ build-jira.sh
@@ -3,4 +3,11 @@ set -euo pipefail
 
 for item in $(cat jira-tags.txt); do
   JIRA_VERSION=${item} docker buildx bake jira
+  if [[ ${item} =~ ^([0-9]+\.[0-9]+)\.[0-9]+$ ]]; then
+    for image in $(JIRA_VERSION=${item} docker buildx bake jira --print | jq -r '.target.jira.tags[]'); do
+      new_image="$(echo "${image}" | sed "s/${item}$/${BASH_REMATCH[1]}/")"
+      docker tag ${image} ${new_image}
+      docker push ${new_image}
+    done
+  fi
 done
