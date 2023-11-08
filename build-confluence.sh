a0f7236f29400b2ecd025f9048d570afcbfdafb5
Nobi <nobi@nobidev.com>
2023-11-08 13:46:55 +0700

    fixup! WIP

--- build-confluence.sh
+++ build-confluence.sh
@@ -3,4 +3,11 @@ set -euo pipefail
 
 for item in $(cat confluence-tags.txt); do
   CONFLUENCE_VERSION=${item} docker buildx bake confluence
+  if [[ ${item} =~ ^([0-9]+\.[0-9]+)\.[0-9]+$ ]]; then
+    for image in $(CONFLUENCE_VERSION=${item} docker buildx bake confluence --print | jq -r '.target.confluence.tags[]'); do
+      new_image="$(echo "${image}" | sed "s/${item}$/${BASH_REMATCH[1]}/")"
+      docker tag ${image} ${new_image}
+      docker push ${new_image}
+    done
+  fi
 done
